90df2d64339644b9ccd1aca8aa7fb88d0120bd01
Jin Hyuk <ekrrdj07@gmail.com>
2024-04-20 23:16:37 +0900

    스티커 Entity에 초기 재고 및 남아있는 재고 컬럼 추가

--- src/main/java/com/example/demo/sticker/domain/Sticker.java
+++ src/main/java/com/example/demo/sticker/domain/Sticker.java
@@ -1,27 +1,32 @@
 package com.example.demo.sticker.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "sticker")
 @NoArgsConstructor
 @Getter
+@AllArgsConstructor
 public class Sticker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     private String name;
     private Integer stock;
-
-    public Sticker(String name, Integer stock) {
+    private Integer initialStock;
+    private Integer price;
+    @Builder
+    public Sticker(String name, Integer initialStock) {
         this.name = name;
-        this.stock = stock;
+        this.initialStock = initialStock;
+        this.stock = initialStock;
     }
+
 
     public Sticker(String name) {
         this.name = name;
     }
+
+    public void updatePrice(Integer price) {this.price = price; }
 }
