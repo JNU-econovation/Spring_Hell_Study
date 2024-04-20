0546e9f2bb5dbfb3ab81d829c080ddd580e05db7
hwangdaesun <hds650588@gmail.com>
2024-04-20 11:35:58 +0900

    feat : 남원 시청 보유 예산 고려하여 초기화

--- src/main/java/com/example/study/DataInit.java
+++ src/main/java/com/example/study/DataInit.java
@@ -1,2 +1,25 @@
-package com.example.study;public class DataInit {
+package com.example.study;
+
+import com.example.study.city.domain.City;
+import com.example.study.city.domain.Region;
+import com.example.study.city.repository.CityRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInit {
+
+    private final CityRepository cityRepository;
+    @PostConstruct
+    void init(){
+
+        City city = City.builder()
+                .name(Region.남원.name())
+                .budget(1000000L)
+                .build();
+        cityRepository.save(city); // 시청 예산 세팅
+    }
+
 }
