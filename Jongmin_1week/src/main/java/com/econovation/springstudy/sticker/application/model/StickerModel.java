17a8a1b6ac514498eb82a00f442cee08836a7e53
KimJongMin <whdals011203@naver.com>
2024-04-21 20:45:29 +0900

    update: purchasable 필드 추가

--- Jongmin_1week/src/main/java/com/econovation/springstudy/sticker/application/model/StickerModel.java
+++ Jongmin_1week/src/main/java/com/econovation/springstudy/sticker/application/model/StickerModel.java
@@ -1,20 +1,24 @@
 package com.econovation.springstudy.sticker.application.model;
 
 import com.econovation.springstudy.common.application.model.Model;
+import lombok.Getter;
 
+@Getter
 public class StickerModel implements Model {
 
     private Long stickerId;
     private String name;
     private Long remain;
-
+    private Long officialPurchasable;
+    private Long nonOfficialPurchasable;
     private Long price;
 
-    public StickerModel(Long stickerId, String name, Long remain, Long price){
+    public StickerModel(Long stickerId, String name, Long remain, Long officialPurchasable, Long nonOfficialPurchasable, Long price){
         this.stickerId = stickerId;
         this.name = name;
         this.remain = remain;
+        this.officialPurchasable = officialPurchasable;
+        this.nonOfficialPurchasable = nonOfficialPurchasable;
         this.price = price;
     }
-
 }
