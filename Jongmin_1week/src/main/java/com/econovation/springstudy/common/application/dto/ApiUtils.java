ba4dcff676759b552234c1493e7eff12480fc916
KimJongMin <whdals011203@naver.com>
2024-04-21 20:57:41 +0900

    update: int status, String message로 필드 변경

--- Jongmin_1week/src/main/java/com/econovation/springstudy/common/application/dto/ApiUtils.java
+++ Jongmin_1week/src/main/java/com/econovation/springstudy/common/application/dto/ApiUtils.java
@@ -1,39 +1,46 @@
 package com.econovation.springstudy.common.application.dto;
 
 import com.econovation.springstudy.common.application.dto.reponse.ApiResponse;
+import org.springframework.http.HttpStatus;
 
 public class ApiUtils {
 
-    public static <T> SuccessBody<T> successBody(String status, String message, T response) {
-        return new SuccessBody<T>(status, message, response);
+    public static <T> SuccessBody<T> successBody(T response) {
+        return new SuccessBody<T>(HttpStatus.OK.value(), response);
     }
 
     public static class SuccessBody<T> implements ApiResponse<T> {
-        private String status;
+        private int status;
         private String message;
         private T data;
 
-        public SuccessBody(String status, String message, T data){
+        public SuccessBody(int status, T data){
+            this.status = status;
+            this.message = HttpStatus.OK.name();
+            this.data = data;
+        }
+
+        public SuccessBody(int status, String message, T data){
             this.status = status;
             this.message = message;
             this.data = data;
         }
 
-        public String getStatus(){ return status; }
+        public int getStatus(){ return status; }
         public String getMessage(){ return message; }
         public T getData(){ return data; }
     }
 
     public static class FailureBody<T> implements ApiResponse<T>{
-        private String status;
+        private int status;
         private String message;
 
-        public FailureBody(String status, String message){
+        public FailureBody(int status, String message){
             this.status = status;
             this.message = message;
         }
 
-        public String getStatus(){ return status; }
+        public int getStatus(){ return status; }
         public String getMessage() { return message; }
     }
 }
