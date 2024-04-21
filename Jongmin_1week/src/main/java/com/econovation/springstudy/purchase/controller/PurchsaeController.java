89ce1335bc216ffd70f84d459ee1b040cae38036
KimJongMin <whdals011203@naver.com>
2024-04-21 20:51:17 +0900

    update: purchaseSticker 반환 타입 QueryStickersResponse로 변경

--- Jongmin_1week/src/main/java/com/econovation/springstudy/purchase/controller/PurchsaeController.java
+++ Jongmin_1week/src/main/java/com/econovation/springstudy/purchase/controller/PurchsaeController.java
@@ -3,7 +3,8 @@ package com.econovation.springstudy.purchase.controller;
 import com.econovation.springstudy.purchase.application.dto.request.PurchaseStickerRequest;
 import com.econovation.springstudy.common.application.dto.reponse.ApiResponse;
 import com.econovation.springstudy.common.application.dto.ApiUtils;
-import com.econovation.springstudy.purchase.application.dto.response.QueryStrickerResponse;
+import com.econovation.springstudy.purchase.application.dto.response.QueryStickerResponse;
+import com.econovation.springstudy.purchase.application.dto.response.QueryStickersResponse;
 import com.econovation.springstudy.purchase.application.usecase.PurchaseStickerUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,16 +16,11 @@ public class PurchsaeController {
 
     private final PurchaseStickerUsecase purchaseStickerUsecase;
 
-//    @GetMapping
-//    public ApiResponse<SuccessBody<List<QueryStrickerResponse>>> getStickers(){
-//
-//    }
-
     @PostMapping("/{stickerId}")
-    public ApiResponse<QueryStrickerResponse> purchaseSticker(
+    public ApiResponse<QueryStickersResponse> purchaseSticker(
             @RequestBody PurchaseStickerRequest request
             ){
-        QueryStrickerResponse response = purchaseStickerUsecase.purchase(request);
-        return ApiUtils.successBody("200", "성공", response);
+        QueryStickersResponse response = purchaseStickerUsecase.purchase(request);
+        return ApiUtils.successBody(response);
     }
 }
