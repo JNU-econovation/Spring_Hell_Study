a3147c4103a14f147d63340416713d2a1707ffd8
KimJongMin <whdals011203@naver.com>
2024-04-21 20:43:48 +0900

    feat: StickerRepository 정의

--- Jongmin_1week/src/main/java/com/econovation/springstudy/sticker/repository/StickerRepository.java
+++ Jongmin_1week/src/main/java/com/econovation/springstudy/sticker/repository/StickerRepository.java
@@ -1,45 +1,16 @@
 package com.econovation.springstudy.sticker.repository;
 
-import com.econovation.springstudy.common.repositroy.Repositroy;
-import com.econovation.springstudy.sticker.application.model.StickerModel;
-import com.econovation.springstudy.sticker.application.model.converter.StickerModelConverter;
 import com.econovation.springstudy.sticker.entity.StickerEntity;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.List;
+import java.util.Optional;
 
-@RequiredArgsConstructor
-@Component
-public class StickerRepository implements Repositroy {
+public interface StickerRepository extends JpaRepository<StickerEntity, Long> {
 
-    /**
-     * Key -> StickerId
-     * Value -> Sticker
-     */
-    private Map<Long, StickerEntity> repository = new HashMap<>();
-    private static Long maxId = 0L;
+    public Optional<StickerEntity> findById(Long id);
 
-    private final StickerModelConverter converter;
-    public StickerEntity add(StickerModel sticker){
-        /**
-         * 저장소에 추가하는 로직
-         */
-        maxId++;
-    }
+    public List<StickerEntity> findAll();
 
-    public StickerModel get(){
-        return converter.from(
-                getRandomSticker()
-        );
-    }
-
-    private StickerEntity getRandomSticker(){
-        Random random = new Random();
-        Long id = random.nextLong(1,maxId);
-        return repository.get(id);
-    }
 
 }
