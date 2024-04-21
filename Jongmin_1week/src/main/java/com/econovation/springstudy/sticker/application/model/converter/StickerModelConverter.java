86e7d4ec533b0b9a57fcfeab68fda4646ced3940
KimJongMin <whdals011203@naver.com>
2024-04-21 20:44:58 +0900

    feat: StickerModelConverter 정의

--- Jongmin_1week/src/main/java/com/econovation/springstudy/sticker/application/model/converter/StickerModelConverter.java
+++ Jongmin_1week/src/main/java/com/econovation/springstudy/sticker/application/model/converter/StickerModelConverter.java
@@ -14,6 +14,8 @@ public class StickerModelConverter {
                 entity.getId(),
                 entity.getName(),
                 entity.getRemain(),
+                entity.getOfficialPurchasable(),
+                entity.getNonOfficialPurchasable(),
                 entity.getPrice()
         );
     }
