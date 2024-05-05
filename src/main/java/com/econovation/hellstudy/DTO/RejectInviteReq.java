c2ff513e8d78138889c36e569b5daceba9cf0e5a
coco3x <inferior3x@gmail.com>
2024-05-05 23:39:58 +0900

    style: 필드 이름 변경

--- src/main/java/com/econovation/hellstudy/DTO/RejectInviteReq.java
+++ src/main/java/com/econovation/hellstudy/DTO/RejectInviteReq.java
@@ -1,4 +1,4 @@
 package com.econovation.hellstudy.DTO;
 
-public record RejectInviteReq(String chatRoomId, String senderId, String receiverId) {
+public record RejectInviteReq(String chatRoomId, String invitingUserId, String invitedUserId) {
 }
