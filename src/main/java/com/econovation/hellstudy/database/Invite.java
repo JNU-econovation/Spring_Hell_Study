0f9c826648550d2c45963d55a241e6372420f324
coco3x <inferior3x@gmail.com>
2024-05-06 21:54:10 +0900

    fix: Invite - 인스턴스 필드 추가
    어떤 채팅방에서의 초대였는지 나타내기 위함

--- src/main/java/com/econovation/hellstudy/database/Invite.java
+++ src/main/java/com/econovation/hellstudy/database/Invite.java
@@ -2,19 +2,20 @@ package com.econovation.hellstudy.database;
 
 import java.util.Objects;
 
-public record Invite(String invitedUserId) {
+public record Invite(String chatRoomId, String invitedUserId) {
 
     @Override
     public boolean equals(Object obj) {
         if (obj == this) return true;
         if (obj instanceof Invite invite) {
-            return (invite.invitedUserId.equals(this.invitedUserId));
+            return (invite.chatRoomId.equals(this.chatRoomId))
+                    && (invite.invitedUserId.equals(this.invitedUserId));
         }
         return false;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.invitedUserId);
+        return Objects.hash(this.chatRoomId, this.invitedUserId);
     }
 }
