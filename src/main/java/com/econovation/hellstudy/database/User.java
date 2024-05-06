1ec3e0aca0313059db0999736535a4cc3789d786
coco3x <inferior3x@gmail.com>
2024-05-06 22:14:56 +0900

    fix: User 필드 nonnull

--- src/main/java/com/econovation/hellstudy/database/User.java
+++ src/main/java/com/econovation/hellstudy/database/User.java
@@ -1,23 +1,17 @@
 package com.econovation.hellstudy.database;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
+import lombok.Getter;
+import lombok.NonNull;
 
+@Getter
 public class User {
     private final Set<Block> blocks;
     private final Set<Invite> invites;
 
-    public User(Set<Block> blocks, Set<Invite> invites) {
+    public User(@NonNull Set<Block> blocks, @NonNull Set<Invite> invites) {
         this.blocks = blocks;
         this.invites = invites;
     }
 
-    public Set<Block> getBlocks() {
-        return blocks;
-    }
-
-    public Set<Invite> getInvites() {
-        return invites;
-    }
 }
