e21f0b007c859679257d3cd4e16516492accba3f
coco3x <inferior3x@gmail.com>
2024-05-07 06:27:08 +0900

    fix: ChatRoomController

--- src/main/java/com/econovation/hellstudy/controller/ChatRoomController.java
+++ src/main/java/com/econovation/hellstudy/controller/ChatRoomController.java
@@ -1,13 +1,22 @@
 package com.econovation.hellstudy.controller;
 
+import com.econovation.hellstudy.DTO.chatroom.ChatRoomListReq;
+import com.econovation.hellstudy.DTO.chatroom.ChatRoomListRes;
+import com.econovation.hellstudy.service.ChatRoomService;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatRoomController {
+    private final ChatRoomService chatRoomService;
+    public ChatRoomController(ChatRoomService chatRoomService) {
+        this.chatRoomService = chatRoomService;
+    }
 
     @GetMapping("/rooms/info")
-    public String getChatRoomsInfo(){
-        return "";
+    public List<ChatRoomListRes> getChatRoomsInfo(@RequestBody ChatRoomListReq chatRoomListReq){
+        return chatRoomService.getChatRooms(chatRoomListReq);
     }
 }
