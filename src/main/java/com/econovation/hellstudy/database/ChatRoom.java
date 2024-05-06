03ae7bdbcf24c646408e197bc7383542866311c3
coco3x <inferior3x@gmail.com>
2024-05-06 22:14:37 +0900

    fix: ChatRoom 필드 nonnull

--- src/main/java/com/econovation/hellstudy/database/ChatRoom.java
+++ src/main/java/com/econovation/hellstudy/database/ChatRoom.java
@@ -1,28 +1,18 @@
 package com.econovation.hellstudy.database;
 
 import java.util.List;
+import lombok.Getter;
+import lombok.NonNull;
 
+@Getter
 public class ChatRoom {
     private final List<ChatMessage> chatMessages;
     private final List<String> guests;
     private final List<GuestInfo> guestInfos;
 
-    public ChatRoom(List<ChatMessage> chatMessages, List<String> guests,
-            List<GuestInfo> guestInfos) {
+    public ChatRoom(@NonNull List<ChatMessage> chatMessages, @NonNull List<String> guests, @NonNull List<GuestInfo> guestInfos) {
         this.chatMessages = chatMessages;
         this.guests = guests;
         this.guestInfos = guestInfos;
-    }
-
-    public List<ChatMessage> getChatMessages() {
-        return chatMessages;
-    }
-
-    public List<String> getGuests() {
-        return guests;
-    }
-
-    public List<GuestInfo> getGuestInfos() {
-        return guestInfos;
     }
 }
