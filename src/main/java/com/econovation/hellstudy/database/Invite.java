f1aab9ac3a28351c5572c82c3b050c553aba2b23
coco3x <inferior3x@gmail.com>
2024-05-06 18:44:18 +0900

    add: Invite equals, hashCode

--- src/main/java/com/econovation/hellstudy/database/Invite.java
+++ src/main/java/com/econovation/hellstudy/database/Invite.java
@@ -1,5 +1,20 @@
 package com.econovation.hellstudy.database;
 
+import java.util.Objects;
+
 public record Invite(String invitedUserId) {
-    //TODO: override equals, hashCode
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj instanceof Invite invite) {
+            return (invite.invitedUserId.equals(this.invitedUserId));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.invitedUserId);
+    }
 }
