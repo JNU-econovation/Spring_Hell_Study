123384f75a91de52e987b43384bac0e143d8037b
coco3x <inferior3x@gmail.com>
2024-05-06 18:43:56 +0900

    add: Block equals, hashCode

--- src/main/java/com/econovation/hellstudy/database/Block.java
+++ src/main/java/com/econovation/hellstudy/database/Block.java
@@ -1,3 +1,19 @@
 package com.econovation.hellstudy.database;
 
-public record Block(String blockedUserId) {}
+import java.util.Objects;
+
+public record Block(String blockedUserId) {
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj instanceof Block block)
+            return (block.blockedUserId.equals(this.blockedUserId));
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.blockedUserId);
+    }
+}
