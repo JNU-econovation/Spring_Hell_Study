ce2fd6633e122f3aee1d4bf1c9ea053e0d10cc85
hwangdaesun <hds650588@gmail.com>
2024-05-08 14:12:46 +0900

    feat : 비동기로 수행 + 해당 스레드 interrupt시 스레드 풀에게 알림 추가

--- src/main/java/com/econovation/hellstudy/chatRoom/service/InviteUserService.java
+++ src/main/java/com/econovation/hellstudy/chatRoom/service/InviteUserService.java
@@ -3,19 +3,28 @@ package com.econovation.hellstudy.chatRoom.service;
 import com.econovation.hellstudy.chatRoom.dto.InviteUserRequest;
 import com.econovation.hellstudy.database.Database;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Service
 @RequiredArgsConstructor
 public class InviteUserService {
 
     private final Database database;
+    @Qualifier(value = "threadPoolTaskExecutor") private final Executor threadPoolTaskExecutor;
 
+    @Async
     public void execute(InviteUserRequest inviteUserRequest){
-        try {
-            database.invite(inviteUserRequest.chatRoomId(),inviteUserRequest.visitorId());
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        CompletableFuture.runAsync(() -> {
+            try {
+                database.invite(inviteUserRequest.chatRoomId(),inviteUserRequest.visitorId());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }, threadPoolTaskExecutor);
     }
 }
