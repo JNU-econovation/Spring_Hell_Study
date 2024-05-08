a274ece02ed25cf4e4e52cd32027f40e5b7a442d
hwangdaesun <hds650588@gmail.com>
2024-05-08 14:08:47 +0900

    feat : 채팅방 생성을 비동기로 수행하고, 별도로 정의된 스레드 풀을 사용

--- src/main/java/com/econovation/hellstudy/chatRoom/service/CreateChatRoomService.java
+++ src/main/java/com/econovation/hellstudy/chatRoom/service/CreateChatRoomService.java
@@ -2,21 +2,34 @@ package com.econovation.hellstudy.chatRoom.service;
 
 import com.econovation.hellstudy.database.Database;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Service
 @RequiredArgsConstructor
 public class CreateChatRoomService {
 
     private final Database database;
+    @Qualifier(value = "threadPoolTaskExecutor") private final Executor threadPoolTaskExecutor;
 
+
+    @Async
     public String execute(String hostId){
         String nextChatRoomId = database.getNextChatRoomId();
-        try {
-            database.createChatRoom(hostId, nextChatRoomId);
-        }catch (InterruptedException e) {
-            throw new RuntimeException();
-        }
+
+            CompletableFuture.runAsync(() -> {
+                        try {
+                            database.createChatRoom(hostId, nextChatRoomId);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }, threadPoolTaskExecutor
+            );
+
         return nextChatRoomId;
     }
 
