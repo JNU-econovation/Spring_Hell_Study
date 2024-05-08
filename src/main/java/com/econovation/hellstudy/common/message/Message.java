b0aed1ec0be68a695dded4fdf976fb0448a20f18
Jin Hyuk <ekrrdj07@gmail.com>
2024-05-08 11:17:12 +0900

    feat: Message의 failLimit final로 수정

--- src/main/java/com/econovation/hellstudy/common/message/Message.java
+++ src/main/java/com/econovation/hellstudy/common/message/Message.java
@@ -3,21 +3,21 @@ package com.econovation.hellstudy.common.message;
 import lombok.Getter;
 
 import java.util.List;
-@Getter
-public class Message {
-     private Integer failCount;
-     private List<Exception> exceptions;
-     private Integer failLimit;
+    @Getter
+    public class Message {
+         private Integer failCount;
+         private List<Exception> exceptions;
+         private final Integer failLimit = 5;
 
-    public void increaseFailCount() {
-        failCount++;
-    }
+        public void increaseFailCount() {
+            failCount++;
+        }
 
-    public boolean isFailLimitExceeded() {
-        return failCount >= failLimit;
-    }
+        public boolean isFailLimitExceeded() {
+            return failCount >= failLimit;
+        }
 
-    public void addException(Exception e) {
-        exceptions.add(e);
-    }
+        public void addException(Exception e) {
+            exceptions.add(e);
+        }
 }
