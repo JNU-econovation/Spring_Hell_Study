ba54f582d15096eb9d1b69d838bc5c8dbb681a2c
Jin Hyuk <ekrrdj07@gmail.com>
2024-05-08 12:15:09 +0900

    feat: 의미없는 작업

--- src/main/java/com/econovation/hellstudy/domains/user/domain/Block.java
+++ src/main/java/com/econovation/hellstudy/domains/user/domain/Block.java
@@ -1,10 +1,12 @@
 package com.econovation.hellstudy.domains.user.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Getter
 public class Block {
     // 차단 하는 사람
     private String blockerId;
