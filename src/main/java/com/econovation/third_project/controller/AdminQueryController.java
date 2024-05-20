59ade23141fdddddc0a89dc4cd525f0b50cdc484
coco3x <inferior3x@gmail.com>
2024-05-20 22:40:23 +0900

    comment: todo

--- src/main/java/com/econovation/third_project/controller/AdminQueryController.java
+++ src/main/java/com/econovation/third_project/controller/AdminQueryController.java
@@ -92,6 +92,14 @@ public class AdminQueryController {
         return ResponseEntity.ok().body(result.join());
     }
 
-    //병렬 스트림 + async
+
+    //TODO:
+    // 병렬 스트림 + async의 장점,
+    // 병렬 스트림, async 각각 적절한 상황,
+    // 서비스 메서드마다 다른 인자가 필요한 경우,
+    // 일부 의존 관계가 있을경우 어떻게 처리할지
+    // 서비스 메서드 맵 어떻게 뺄지,
+    // 적절한 스레드 수 알아보기,
+    // 자바 비동기 기초,
 
 }
