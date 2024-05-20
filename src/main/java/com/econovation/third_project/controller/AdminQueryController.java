b9cca04c994166baf61d2eb6bf936143668eb661
hwangdaesun <hds650588@gmail.com>
2024-05-20 14:22:15 +0900

    feat : Controller에서 각 QueryService로 동기로 실행

--- src/main/java/com/econovation/third_project/controller/AdminQueryController.java
+++ src/main/java/com/econovation/third_project/controller/AdminQueryController.java
@@ -1,19 +1,19 @@
 package com.econovation.third_project.controller;
 
-import com.econovation.third_project.service.ApplicantQueryService;
+import com.econovation.third_project.dto.*;
+import com.econovation.third_project.service.*;
 import com.econovation.third_project.database.Database;
 import com.econovation.third_project.database.Registration;
-import com.econovation.third_project.service.HopeFieldQueryService;
-import com.econovation.third_project.service.MajorQueryService;
-import com.econovation.third_project.service.SupportPathQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +23,7 @@ public class AdminQueryController {
     private final MajorQueryService majorQueryService;
     private final SupportPathQueryService supportPathQueryService;
     private final HopeFieldQueryService hopeFieldQueryService;
+    private final DesiredTimeQueryService desiredTimeQueryService;
 
     // 예시 코드
     @PostMapping("/registration")
@@ -30,14 +31,27 @@ public class AdminQueryController {
         database.register(registration);
         return ResponseEntity.ok().build();
     }
-    @GetMapping("/admin")
-    public ResponseEntity<Registration> getAdmin(String userId) {
-        applicantQueryService.execute();
-        majorQueryService.execute();
-        hopeFieldQueryService.execute();
-        supportPathQueryService.execute();
-        hopeFieldQueryService.execute();
-        return ResponseEntity.ok().body(database.getRegistration(userId));
+
+
+    @GetMapping("/admin/{applicants}/{majors}/{programmers}/{path}/{desiredTime}")
+    public ResponseEntity<AdminQueryResponse> getAdmin(
+            @PathVariable(required = false) String applicants, @PathVariable(required = false) String majors,
+            @PathVariable(required = false) String programmers, @PathVariable(required = false) String path, @PathVariable(required = false) String desiredTime) {
+
+
+        List<ApplicantDTO> applicantDTOS = applicants.isEmpty() ? null : applicantQueryService.execute();
+        List<MajorDTO> majorDTOS = majors.isEmpty() ? null : majorQueryService.execute();
+        List<ProgrammerFieldDTO> programmerDTOS = programmers.isEmpty() ? null : hopeFieldQueryService.execute();
+        List<PathDTO> pathDTOS = path.isEmpty() ? null : supportPathQueryService.execute();
+        List<DesiredTimeDTO> desiredTimeDTOS = desiredTime.isEmpty() ? null :  desiredTimeQueryService.execute();
+
+        return ResponseEntity.ok().body(AdminQueryResponse.builder()
+                .applicants(applicantDTOS)
+                .majors(majorDTOS)
+                .programmers(programmerDTOS)
+                .path(pathDTOS)
+                .desiredTime(desiredTimeDTOS)
+                .build());
     }
 
 }
